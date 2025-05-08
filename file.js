753525f6eee4a6aaf80e91a77d49b9471576983a
Danishdbu <mohddanishdbu@gmail.com>
2025-05-08 13:51:15 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,6 @@
+console.log("Coding");
+const fs = require('fs');
+fs.writeFile("output.txt","Writing File", (err) =>{
+    if (err) console.log("Error occurred");
+    else console.log("File Written successfully");
+})
