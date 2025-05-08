bf092923388b93b2c162684285fafd9fcbc91dfe
Danishdbu <mohddanishdbu@gmail.com>
2025-05-08 15:11:09 +0530

    "sending response"

---
+++ file3.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Coding</title></head>');
+    res.write('<body><h1>Welcome my website</h1></body>');
+    res.write('</html>');
+    res.end();
+   
+});
+
+server.listen(3000,() =>{
+    console.log(`Server running on address http://localhost:3000`);
+});
