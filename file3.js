539f8cc008c215cb26c7fe3bb4bef5f678ab7f97
Danishdbu <mohddanishdbu@gmail.com>
2025-05-08 15:34:10 +0530

    "response based on url"

--- file3.js
+++ file3.js
@@ -3,12 +3,29 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
+    if(req.url === '/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Coding</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+
+    }else if(req.url === '/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Coding</title></head>');
+        res.write('<body><h1>Welcome about</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>Coding</title></head>');
     res.write('<body><h1>Welcome my website</h1></body>');
     res.write('</html>');
     res.end();
+    
    
 });
 
