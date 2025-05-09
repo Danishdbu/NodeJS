a733de9308db603280d91358e726c449ee816acd
Danishdbu <mohddanishdbu@gmail.com>
2025-05-09 14:47:42 +0530

    routing in express

--- expressjs/app.js
+++ expressjs/app.js
@@ -4,15 +4,19 @@ const requestHandler = require('./user');
 
 const app = express();
 
-app.use((req,res,next)=>{
+app.use("/",(req,res,next)=>{
   console.log("Came in first middleware",req.url,req.method);
+  res.send("<p>welcome in first middleware</p>");
   next();
 });
-
-app.use((req,res,next)=>{
+app.use("/",(req,res,next)=>{
+  console.log("Came in another middleware",req.url,req.method);
+  res.send("<p>welcome in another middleware</p>");
+});
+app.use("/submit-details",(req,res,next)=>{
   console.log("Came in second middleware",req.url,req.method);
   res.send("<p>welcome in middleware</p>");
-});
+}); 
 
 
 
