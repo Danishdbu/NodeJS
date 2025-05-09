3f389134aed55c3743f96a21c67e81944b4b77d4
Danishdbu <mohddanishdbu@gmail.com>
2025-05-09 12:53:38 +0530

    Start expressJS

--- expressjs/app.js
+++ expressjs/app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req,res)=>{
     console.log(req);
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
