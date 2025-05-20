772eafee40d37a105e238faea6274776d35fb0f1
Danishdbu <mohddanishdbu@gmail.com>
2025-05-20 12:36:14 +0530

    adding 404 error page

--- airbnb/app.js
+++ airbnb/app.js
@@ -7,9 +7,11 @@ const app = express();
 app.use(express.urlencoded());
 
 app.use(userRouter);
-
 app.use(hostRouter);
 
+app.use((req, res, next) =>{
+  res.status(404).send("<h1>404 Your page is not found on airbnb</h1>");
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
